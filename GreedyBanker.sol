cbf2d5a3a162aa401014c9b5afcebdb9ff5958f2
charmingdata <94773218+charmingdata@users.noreply.github.com>
2023-05-24 18:38:08 -0400

    Update GreedyBanker.sol

--- GreedyBanker.sol
+++ GreedyBanker.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.17;
 
-// The original contract can be found in reddit link below. But it has been thoroughly modified.
+// The original contract can be found in the reddit link below. But it has been thoroughly modified.
 // https://www.reddit.com/r/solidity/comments/13gzpob/new_solidity_dev_code_review/?utm_source=share&utm_medium=web2x&context=3
 
 contract GreedyBanker {
