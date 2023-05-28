47a97fe7245fd44e4b52b46dff1754d1d8d2ac38
charmingdata <94773218+charmingdata@users.noreply.github.com>
2023-05-28 01:18:53 -0400

    Create DonationToEducator.sol

---
+++ DonationToEducator.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// An informative description of Events: https://ethereum.stackexchange.com/a/56880/120832
+
+contract Donation {
+    address payable public onlineEducator;
+
+    event LogData(
+        uint256 indexed amount,
+        string indexed reason,
+        address donatorAddress,
+        uint256 timestamp
+    );
+
+    constructor() payable {
+        // Set the Online Educator as the person who instantiated the contract
+        onlineEducator = payable(msg.sender);
+    }
+
+    function offerDonation(string calldata donationReason) external payable {
+        // Emit the event LogData
+        emit LogData(msg.value, donationReason, msg.sender, block.timestamp);
+        // Send the ether to the Online Educator
+        onlineEducator.transfer(msg.value);
+    }
+}
